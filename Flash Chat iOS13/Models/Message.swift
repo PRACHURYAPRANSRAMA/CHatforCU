e3b12e386227a7869eafa870a3845c4511957a48
PRACHURYAPRANSARMA <pps11sarma@gmail.com>
2022-05-16 17:54:23 +0530

    first commit

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by PRACHURYA PRAN SARMA'SVELAR on 15/04/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
